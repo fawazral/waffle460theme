3e57500c4070b91a92aca0662441e6bf074af15a
zh460 <zohairh.hussain@gmail.com>
2016-03-19 23:17:19 -0400

    style.css

--- footer.php
+++ footer.php
@@ -16,17 +16,17 @@
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="container">
 		
-		<div class="site-info">
+		<div id="footer-menu">
+				<?php wp_nav_menu( array( 'theme_location' => 'secondary',
+					'menu_class' => 'foot-menu' ) ); ?>
+		</div><!--#footer-menu -->
+		
+		<div class="info-site">
 			<p class="copyright">&copy; <?php echo date('Y'); ?> 
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'waffle460theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'waffle460theme' ), 'WordPress' ); ?></a> All Rights Reserved.</p>
 			<p class="credits">
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'waffle460theme' ), 'waffle460theme', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
-		
-		<div id="footer-menu">
-				<?php wp_nav_menu( array( 'theme_location' => 'secondary',
-					'menu_class' => 'foot-menu' ) ); ?>
-		</div><!--#footer-menu -->
 					
 	</footer><!-- #colophon -->
 </div><!-- #page -->
