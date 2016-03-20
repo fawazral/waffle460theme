5959ebb27085d1bf5b59370085558b4e415b3aa1
zh460 <zohairh.hussain@gmail.com>
2016-03-20 15:19:32 -0400

    j

--- header.php
+++ header.php
@@ -42,7 +42,7 @@
 		</div><!-- .site-branding -->
 		
 		<div id="logo">
-		 echo "<img src='/images/logo.png'>";
+		 echo <img src='/images/logo.png'>
 		</div><!-- #header logo -->
 
 		<div id="header-menu">
